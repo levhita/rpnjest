8a2fd063f9aaebc0aeb81fd7d14f570ebfb8023b
Levhita <levhita@gmail.com>
2018-10-28 22:44:42 -0600

    first functional version

--- js/evalPolish.js
+++ js/evalPolish.js
@@ -1,3 +1,13 @@
-const evalPolish = (expression) => 4;
+const evalPolish = (expression) => {
+    const tokens = expression.split(" ");
+    const stack = [];
+    while( token = tokens.shift() ) {
+       
+        if ( !isNaN(token) ) {
+            stack.push(parseInt(token));
+        }
+    }
+    return stack.pop();
+};
 
 module.exports = evalPolish;
--- test/evalPolish.spec.js
+++ test/evalPolish.spec.js
@@ -1,7 +1,7 @@
 const evalPolish = require('../js/evalPolish');
 
 describe('evalPolish', () => {
-    test(' 2 2 + should return 4',()=>{
-        expect(evalPolish('2 2 +')).toBe(4);
+    test(' "2" should return 2',()=>{
+        expect(evalPolish('2')).toBe(2);
     });
 });
