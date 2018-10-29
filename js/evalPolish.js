5f2c9b46285c2065448f39f18ea6ca9932cc2549
Levhita <levhita@gmail.com>
2018-10-28 22:48:57 -0600

    handle addition

--- js/evalPolish.js
+++ js/evalPolish.js
@@ -2,8 +2,13 @@ const evalPolish = (expression) => {
     const tokens = expression.split(" ");
     const stack = [];
     while( token = tokens.shift() ) {
-       
-        if ( !isNaN(token) ) {
+        if ( isNaN(token) ){
+            if (token == '+'){
+                const first = stack.pop();
+                const second = stack.pop();
+                stack.push( first + second );
+            }
+        } else  {
             stack.push(parseInt(token));
         }
     }
--- test/evalPolish.spec.js
+++ test/evalPolish.spec.js
@@ -4,4 +4,7 @@ describe('evalPolish', () => {
     test(' "2" should return 2',()=>{
         expect(evalPolish('2')).toBe(2);
     });
+    test(' "2 2 +" should return 4',()=>{
+        expect(evalPolish('4')).toBe(4);
+    });
 });
