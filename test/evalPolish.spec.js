d81156785fb2440b653dc4183e27e2d702ef2bf2
Levhita <levhita@gmail.com>
2018-10-30 22:49:13 -0600

    ordered tests

--- test/evalPolish.spec.js
+++ test/evalPolish.spec.js
@@ -1,10 +1,10 @@
 const evalPolish = require('../js/evalPolish');
 
-describe('evalPolish', () => {
+describe('Basic Operations', () => {
     test(' "2" should return 2',()=> {
         expect(evalPolish('2')).toBe(2);
     });
-    
+
     test(' "2 2 +" should return 4',()=> {
         expect(evalPolish('2 2 +')).toBe(4);
     });
@@ -20,31 +20,34 @@ describe('evalPolish', () => {
     test(' "2 2 /" should return 1',()=> {
         expect(evalPolish('2 2 /')).toBe(1);
     });
+});
 
+describe('Error Handling', () => {
     test(' "¿" should throw an Error', ()=> {
         expect( () => evalPolish('¿') ).toThrow(Error);
     });
 
-    test(' "2 4 /" should return 1',()=> {
-        expect(evalPolish('2 4 /')).toBe(0.5);
-    });
-    
-    test(' "4 2 /" should return 1',()=> {
-        expect(evalPolish('4 2 /')).toBe(2);
-    });
-
-    test(' "0.1 0.2 +" should return 0.3',()=> {
-        expect(evalPolish('0.1 0.2 +')).toBeCloseTo(0.3);
-    });
-    
-    test(' "+" should throw an Error', () => {
-        expect(() => evalPolish('+')).toThrow(Error);
-    });
-    
     test(' "2 2 2 +" should throw an Error', () => {
         expect(() => evalPolish('+')).toThrow(Error);
     });
 
+    test(' "+" should throw an Error', () => {
+        expect(() => evalPolish('+')).toThrow(Error);
+    });
+});
+
+describe('Order does matter', () => {
+    test(' "2 4 /" should return 0.5', () => {
+        expect(evalPolish('2 4 /')).toBe(0.5);
+    });
+    
+    test(' "4 2 /" should return 2',()=> {
+        expect(evalPolish('4 2 /')).toBe(2);
+    });
+});
+
+    
+describe('Edge Cases', () => {
     test(' "-1 2 *" should return -2', () => {
         expect(evalPolish('-1 2 *')).toBe(-2);
     });
@@ -66,5 +69,5 @@ describe('evalPolish', () => {
 
     test(' "10 log" should return 1', () => {
         expect(evalPolish('10 log')).toBeCloseTo(1);
-    });
+    });  
 });
